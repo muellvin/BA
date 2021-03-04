1c0f53081570d3617ab9accc52b4462c6583ced7
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-03-04 22:34:32 +0100

    crossection features

--- crosssection.py
+++ crosssection.py
@@ -1,1 +1,41 @@
 #crosssection calculation
+class crosssection():
+#a crosssection is defined as a list of lines
+    def __init__(self):
+        self.lines = []
+
+    def addline(self, line):
+        self.lines.append(line)
+# methods to calculate properties of total crossection
+    def get_area_tot(self):
+        a=0
+        for i in self.lines:
+            a = a + i.get_area_tot()
+        return a
+
+    def get_z_center_tot(self):
+        weighted_a = 0
+        for i in self.lines:
+            weighted_a = weighted_a + i.get_area_tot()*i.get_z_center_tot()
+        return weighted_a/self.get_area_tot()
+
+    def get_y_center_tot(self):
+        weighted_a = 0
+        for i in self.lines:
+            weighted_a = weighted_a + i.get_area_tot()*i.get_y_center_tot()
+        return weighted_a/self.get_area_tot()
+
+    def get_iy_tot(self):
+        z_s = self.get_z_center_tot()
+        iy_tot = 0
+        for i in self.lines:
+            iy_tot = iy_tot + i.get_iy_tot() + (z_s-i.get_z_center_tot())**2 * i.get_area_tot()
+        return iy_tot
+
+    def get_iz_tot(self):
+        y_s = self.get_y_center_tot()
+        iz_tot = 0
+        for i in self.lines:
+            iz_tot = iz_tot + i.get_iz_tot() + (y_s-i.get_y_center_tot())**2 * i.get_area_tot()
+        return iz_tot
+#methods to calculate properties of reduced crossection
