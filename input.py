93c2ed7079623ba6b2958e85f84a36dd84cbcacf
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-03-04 07:59:37 +0100

    input from command

--- input.py
+++ input.py
@@ -1,1 +1,16 @@
 #input
+def userprompt():
+    print('Value of b_sup? [mm]')
+    b_sup = input()
+    print('Value of b_inf? [mm]')
+    b_inf = input()
+    print('Value of h? [mm]')
+    h = input()
+    print('Value of M_Ed? [kNm]')
+    M_Ed= input()
+    print('Value of V_Ed? [kN]')
+    V_Ed = input()
+    print('Value of T_Ed? [kNm]')
+    T_Ed = input()
+    input_data = [b_sup, b_inf, h, M_Ed, V_Ed, T_Ed]
+    return input_data
