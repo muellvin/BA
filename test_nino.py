f8bfe907ffbd16d5a3e0e0c27bec9d9d206ab452
muellvin <78660277+muellvin@users.noreply.github.com>
2021-03-05 14:07:51 +0100

    asd

--- test_nino.py
+++ test_nino.py
@@ -1,16 +1,17 @@
 #This is a test script
 
-import geometry_classes as gc
-import crosssection as cs
+from classes import point
+from classes import line
+from classes import crosssection
 
-a = gc.point(0,0)
-b = gc.point(0,2)
-e = gc.point(2,0)
-d = gc.point(2,2)
-line1 = gc.line(0,a,b,1)
-line2 = gc.line(0,e,d,1)
+a = point.point(0,0)
+b = point.point(0,2)
+e = point.point(2,0)
+d = point.point(2,2)
+line1 = point.line(0,a,b,1)
+line2 = point.line(0,e,d,1)
 
-test_cs = cs.crosssection()
+test_cs = crosssection.crosssection()
 test_cs.addline(line1)
 test_cs.addline(line2)
 
