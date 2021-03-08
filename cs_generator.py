e9d520f04d4f16ad3745d4ded81d9cd3285160fb
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-03-08 11:23:00 +0100

    bli bla blup

--- cs_generator.py
+++ cs_generator.py
@@ -1,1 +1,16 @@
 #crosssection generation
+class cs_collection():
+    """This is a collection of crossections."""
+
+    #constructor creates four empty crosssection lists
+    def __init__(self, initial_cs, current_cs, last_cs, best_cs):
+        self.initial_cs = crosssection()
+        self.current_cs = crosssection()
+        self.last_cs = crosssection()
+        self.best_cs = crosssection()
+
+    def create_initial(b_sup, b_inf, h, t_sup, t_inf, t_side):
+        pass
+
+    def add_stiffener(line, norm_position, stiffener):
+        pass
---
+++ data/constants.py
@@ -0,0 +1,8 @@
+import math
+
+#list of constants
+
+E = 210000
+f_y = 235
+G = 81000
+t_y = f_y / math.sqrt(3)
