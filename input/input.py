3af94afc2945dfe51527b40930e25eb5a12f89b3
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-03-10 22:16:07 +0100

    bliblablup

--- input/input.py
+++ input/input.py
@@ -3,24 +3,24 @@ import data
 
 print('Value of b_sup? [mm]')
 b_sup = input()
-data.input_data.append("b_sup": b_sup)
+data.input_data.update({"b_sup": b_sup})
 
 print('Value of b_inf? [mm]')
 b_inf = input()
-data.input_data.append("b_inf": b_inf)
+data.input_data.update({"b_inf": b_inf})
 
 print('Value of h? [mm]')
 h = input()
-data.input_data.append("h": h)
+data.input_data.update({"h": h})
 
 print('Value of M_Ed? [kNm]')
 M_Ed= input()
-data.input_data.append("M_Ed": M_Ed)
+data.input_data.update({"M_Ed": M_Ed})
 
 print('Value of V_Ed? [kN]')
 V_Ed = input()
-data.input_data.append("V_Ed": V_Ed)
+data.input_data.update({"V_Ed": V_Ed})
 
 print('Value of T_Ed? [kNm]')
 T_Ed = input()
-data.input_data.append("T_Ed": T_Ed)
+data.input_data.update({"T_Ed": T_Ed})
