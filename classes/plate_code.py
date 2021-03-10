c733cf2916fd9faa4d7cfc4e53f9e5d1277b0e15
muellvin <78660277+muellvin@users.noreply.github.com>
2021-03-10 12:20:24 +0100

    phu

--- classes/plate_code.py
+++ classes/plate_code.py
@@ -3,14 +3,25 @@
 class plate_code():
     """This code reveals, where the specified plate of the crosssection is."""
 
-    def __init__(self, pl_position, pl_type, st_number, st_pl_position):
+    def __init__(self, pl_position, pl_type, tpl_number, st_number, st_pl_position):
         self.pl_position = pl_position
         self.pl_type = pl_type
+        self.tpl_number = tpl_number
         self.st_number = st_number
         self.st_pl_position = st_pl_position
 
-#pl_position: clockwise, starting with the (top) track plate from 1 to 4
-#pl_type: plates of initial crossection: 0, plates of stiffener: 1
-#st_number: clockwise starting with top left hand corner with number 1
-#st_pl_position: similar to pl_position, but with stiffener plates
-#main plate is Nr.1, thus if st_pl_position=1, pl_type=0. 
+#pl_position:
+    #clockwise, starting with the (top) track plate from 1 to 4
+#pl_type:
+    #plates of trapezoid: 0, plates of stiffener: 1
+#tpl_number: trapezoid plate number
+    #the lines of the trapezoid are numbered themselves also starting at top left and going in clockwise direction
+    #they do not match to the st_number
+#st_number:
+    #clockwise starting with top left hand corner with number 1 and ending again at top left corner
+    #this number is unique for every stiffener
+    #the line of the trapezoid included by the stiffener has this number as well
+    #the trapezoid lines not included by stiffeners have 0
+#st_pl_position:
+    #1 is the trapezoid line, 2,3,4 are lines of the stiffener numbered in clockwise direction
+    #trapezoid lines not included by the stiffeners have 0
---
+++ classes/stiffener.py
@@ -0,0 +1,68 @@
+#not really a class
+#has methods for creating stiffeners of the type crosssection
+
+import math
+
+
+    """not finished!!! needs correction for center"""
+    def create_stiffener_globalcoordinates(center, angle, width_top, width_bottom, height, t):
+        assert width_top >= width_bottom: "width out of bound or wrong way around"
+        half_width_diff = width_top - width_bottom
+        length_side = math.sqrt(half_width_diff**2 + height**2)
+        own_angle = math.atan(half_width_diff / height)
+
+        #create plate 2
+        a2 = point.point(0,0)
+        b2 = point.point(math.sin(own_angle-angle)*length_side, math.cos(own_angle-angle)*length_side)
+        line2 = line.line(a2, b2, t)
+
+        #create plate 3
+        a3 = b2
+        b3 = point.point(a3.y + math.cos(angle)*width_bottom, a3.z + math.sin(angle)*width_bottom)
+        line3 = line.line(a3, b3, t)
+
+        #create plate 4
+        a4 = b3
+        b4 = point.point(math.cos(angle)*width_top, math.sin(angle)*width_top)
+        line3 = line.line(a4, b5, t)
+
+        stiffener_global = crosssection.crosssection()
+        #add the lines to itself
+        stiffener_global.lines.addline(line2)
+        stiffener_global.lines.addline(line3)
+        stiffener_global.lines.addline(line4)
+        return stiffener_global
+
+
+    def create_stiffener_owncoordinates(width_top, width_bottom, height, t):
+        assert width_top >= width_bottom: "width out of bound or wrong way around"
+        half_width_diff = width_top - width_bottom
+        length_side = math.sqrt(half_width_diff**2 + height**2)
+        own_angle = math.atan(half_width_diff / height)
+
+        #create plate 2
+        a2 = point.point(-width_top/2,0)
+        b2 = point.point(a2.y + math.sin(own_angle)*length_side, math.cos(own_angle)*length_side)
+        line2 = line.line(a2, b2, t)
+
+        #create plate 3
+        a3 = b2
+        b3 = point.point(a3.y + width_bottom, a3.z)
+        line3 = line.line(a3, b3, t)
+
+        #create plate 4
+        a4 = b3
+        b4 = point.point(width_top/2, 0)
+        line3 = line.line(a4, b5, t)
+
+        stiffener_local = crosssection.crosssection()
+        #add the lines to itself
+        stiffener_local.lines.addline(line2)
+        stiffener_local.lines.addline(line3)
+        stiffener_local.lines.addline(line4)
+
+        
+    def get_i_along_stiffener(width_top, width_bottom, height, t):
+        stiffener_local = create_stiffener_local(width_top, width_bottom, height, t)
+        i_along = stiffener_local.get_i_y_tot()
+        return i_along
