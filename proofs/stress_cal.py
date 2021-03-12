d702020bb860a475d7bbca64fb91b77c3008e201
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-03-12 14:42:49 +0100

    shear forced debugged

--- proofs/stress_cal.py
+++ proofs/stress_cal.py
@@ -1,18 +1,19 @@
-def get_sigma_sup(cs, line, m_y):
+#imports
+import math
+
+def get_sigma_sup(cs, code, m_y):
+    line = cs.get_line(code)
     i_y = cs.get_i_y_tot()
-    print(i_y)
     z_center = cs.get_center_z_tot()
-    print(z_center)
     if line.a.z <= line.b.z:
         z_sup = line.a.z
     else:
         z_sup = line.b.z
-    print(z_sup)
-    print(m_y)
     sigma_sup = m_y * (z_sup-z_center) / i_y
     return sigma_sup
 
-def get_sigma_inf(cs, line, m_y):
+def get_sigma_inf(cs, code, m_y):
+    line = cs.get_line(code)
     i_y = cs.get_i_y_tot()
     z_center = cs.get_center_z_tot()
     if line.a.z >= line.b.z:
@@ -22,7 +23,8 @@ def get_sigma_inf(cs, line, m_y):
     sigma_inf = m_y / i_y * (z_inf-z_center)
     return sigma_inf
 
-def get_sigma_sup_red(cs, line, m_y):
+def get_sigma_sup_red(cs, code, m_y):
+    line = cs.get_line(code)
     i_y = cs.get_i_y_red()
     z_center = cs.get_center_z_red()
     if line.p1.z <= line.p2.z:
@@ -32,7 +34,8 @@ def get_sigma_sup_red(cs, line, m_y):
     sigma_sup_red = m_y / i_y * (z_sup-z_center)
     return sigma_sup_red
 
-def get_sigma_inf_red(cs, line, m_y):
+def get_sigma_inf_red(cs, code, m_y):
+    line = cs.get_line(code)
     i_y = cs.get_i_y_red()
     z_center = cs.get_center_z_red()
     if line.p1.z >= line.p2.z:
@@ -43,17 +46,14 @@ def get_sigma_inf_red(cs, line, m_y):
     return sigma_inf_red
 
 #returns the absolute value of the resulting shear force in a plate of the crosssection
-def get_tau_int(cs, line):
-    return abs(get_tau_int_t() + get_tau_int_qy())
+def get_tau_int(cs, code, v_ed, t_ed):
+    return abs(get_tau_int_t(cs, code, t_ed) + get_tau_int_qy(cs, code, v_ed))
 
 #shear stresses positive in counterclockwise direction
-def get_tau_int_qy(cs, line, v_ed):
-    tau_int = 0
+def get_tau_int_qy(cs, code, v_ed):
+    line = cs.get_line(code)
     y_center = line.get_center_y_tot()
-    sideplate_slope = (line.a.y - line.b.y)/(line.a.z - line.b.z)
-    code = line.code.pl_position
-    assert code > 0 and code < 5, "Invalid plate code"
-    if code == 1 or code ==3:
+    if code.pl_position == 1 or code.pl_position ==3:
         if y_center == 0:
             #assumption should be reconsidered
             tau_int = 0
@@ -61,18 +61,23 @@ def get_tau_int_qy(cs, line, v_ed):
             tau_int = get_v_horizontal_plates(cs, line, v_ed)
         else:
             tau_int = -1*get_v_horizontal_plates(cs, line, v_ed)
-    elif code == 2:
-        tau_int = - v_ed / 2* math.cos(math.atan(sideplate_slope))
-    elif code == 4:
-        tau_int = v_ed / 2* math.cos(math.atan(sideplate_slope))
+    elif code.pl_position == 2:
+        sideplate_slope = (line.a.y - line.b.y)/(line.a.z - line.b.z)
+        tau_int = - v_ed / (2* math.cos(math.atan(sideplate_slope)))
+    elif code.pl_position == 4:
+        sideplate_slope = (line.a.y - line.b.y)/(line.a.z - line.b.z)
+        tau_int = v_ed / (2* math.cos(math.atan(sideplate_slope)))
+    print("Tau_Q = " + str(tau_int))
+    return tau_int
 
 #shear stresses positive in counterclokwise direction
-def get_tau_int_t(cs, line, tor):
+def get_tau_int_t(cs, code, tor):
+    line = cs.get_line(code)
     azero = cs.get_azero()
-    t = line.t
     l = line.get_length_tot()
-    tau = tor / (2*azero*t)
+    tau = tor / (2*azero)
     tau_int = tau * l
+    print ("Tau_T = " + str(tau_int))
     return tau_int
 
 def get_v_horizontal_plates(cs, line, v_ed):
--- tests/test_input.py
+++ tests/test_input.py
@@ -8,17 +8,17 @@ import sys
 sys.path.append('C:/Users/Nino/Google Drive/Studium/FS 2021/Bachelorarbeit/BA')
 
 #imports
-import input.input
+#import input.input
 import data
 import initial_cs as ics
 from classes import crosssection as cs
-from classes import line as ln
+from classes import plate_code as plcd
 from proofs import stress_cal as str_cal
 
 #assign input to variables
-b_sup = data.input_data["b_sup"]
-b_inf = data.input_data["b_inf"]
-h = data.input_data["h"]
+b_sup = 4000 #data.input_data["b_sup"]
+b_inf = 2000 #data.input_data["b_inf"]
+h = 1500 #data.input_data["h"]
 
 #create initial cross section
 test_cs = ics.create_initial_cs(b_sup, b_inf, h)
@@ -40,14 +40,14 @@ A_0 = test_cs.get_azero()
 print("A_0 = " + str(A_0))
 
 #assign load input to variables
-my = data.input_data["M_Ed"]
+my = 2500 * 10**6 #data.input_data["M_Ed"]
+vz = 500 * 10**3 #data.input_data["V_Ed"]
+tx = 300 * 10**6 #data.input_data["T_Ed"]
 
 #testing of stresscal
-test_line = 0
-for line in test_cs.lines:
-    if line.code.pl_position == 1:
-        test_line = line
-    else:
-        pass
-sigma_sup_test_cs = str_cal.get_sigma_sup(test_cs, test_line, my)
+code = plcd.plate_code(1,0,0,0,0)
+sigma_sup_test_cs = str_cal.get_sigma_sup(test_cs, code, my)
 print("sigma_sup = " + str(sigma_sup_test_cs))
+
+v_web_r = str_cal.get_tau_int(test_cs, code, vz, tx)
+print("V_web_r =" + str(v_web_r))
