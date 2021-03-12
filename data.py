3684385174163471b4ea93e2607d0763ddc32990
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-03-12 08:45:16 +0100

    blablabla

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
-import math
-
 #list of constants
 constants = {"E": 210000, "f_y": 235, "G": 81000}
 
+#list of input
 input_data = {}
--- tests/test_input.py
+++ tests/test_input.py
@@ -2,11 +2,7 @@
 
 #this statement adds the project path to the path where the python interpreter looks
 #for packages to import
-#it is also possible to add this to the environment variable PYTHONPATH in the windows settings
-#as far as I understand this needs to be done individually for each user, because the documents variable
-#saved on differen paths...
-#this could be a big issue for distribution...
-#However it makes our inputs work :-)
+#if we start the program from main, this should not be an issue
 import sys
 #just add the directory where the BA folder is on your computer
 sys.path.append('C:/Users/Nino/Google Drive/Studium/FS 2021/Bachelorarbeit/BA')
