d21b526115953ef29544570518fc3002f69b21e9
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-03-12 10:56:22 +0100

    stresscal debugging #1

--- input/input.py
+++ input/input.py
@@ -2,25 +2,25 @@
 import data
 
 print('Value of b_sup? [mm]')
-b_sup = input()
+b_sup = float(input())
 data.input_data.update({"b_sup": b_sup})
 
 print('Value of b_inf? [mm]')
-b_inf = input()
+b_inf = float(input())
 data.input_data.update({"b_inf": b_inf})
 
 print('Value of h? [mm]')
-h = input()
+h = float(input())
 data.input_data.update({"h": h})
 
 print('Value of M_Ed? [kNm]')
-M_Ed= input()
+M_Ed= 10**6 * float(input())
 data.input_data.update({"M_Ed": M_Ed})
 
 print('Value of V_Ed? [kN]')
-V_Ed = input()
+V_Ed = 10**3 * float(input())
 data.input_data.update({"V_Ed": V_Ed})
 
 print('Value of T_Ed? [kNm]')
-T_Ed = input()
+T_Ed = 10**6 * float(input())
 data.input_data.update({"T_Ed": T_Ed})
--- tests/test_input.py
+++ tests/test_input.py
@@ -12,19 +12,42 @@ import input.input
 import data
 import initial_cs as ics
 from classes import crosssection as cs
+from classes import line as ln
+from proofs import stress_cal as str_cal
 
-#test code
-b_sup = float(data.input_data["b_sup"])
-b_inf = float(data.input_data["b_inf"])
-h = float(data.input_data["h"])
+#assign input to variables
+b_sup = data.input_data["b_sup"]
+b_inf = data.input_data["b_inf"]
+h = data.input_data["h"]
 
 #create initial cross section
 test_cs = ics.create_initial_cs(b_sup, b_inf, h)
+for line in test_cs.lines:
+    line.t = 20
 
 #get geometry values of cross section
 y_center = test_cs.get_center_y_tot()
 print("y_center = " + str(y_center))
 z_center = test_cs.get_center_z_tot()
-print("z_center =" + str(z_center))
+print("z_center = " + str(z_center))
 A = test_cs.get_area_tot()
-print("Area =" + str(A))
+print("Area = " + str(A))
+I_y = test_cs.get_i_y_tot()
+print("I_y = " + str(I_y))
+I_z = test_cs.get_i_z_tot()
+print("I_z = " + str(I_z))
+A_0 = test_cs.get_azero()
+print("A_0 = " + str(A_0))
+
+#assign load input to variables
+my = data.input_data["M_Ed"]
+
+#testing of stresscal
+test_line = 0
+for line in test_cs.lines:
+    if line.code.pl_position == 1:
+        test_line = line
+    else:
+        pass
+sigma_sup_test_cs = str_cal.get_sigma_sup(test_cs, test_line, my)
+print("sigma_sup = " + str(sigma_sup_test_cs))
