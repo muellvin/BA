6a40cbd8dd7b238553592aa6d1ed9ae7d9878fd4
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-03-23 13:37:32 +0100

    debugging

--- tests/test_substantiate.py
+++ tests/test_substantiate.py
@@ -2,11 +2,12 @@ import sys
 #just add the directory where the BA folder is on your computer
 sys.path.append('C:/Users/Nino/Google Drive/Studium/FS 2021/Bachelorarbeit/BA')
 
-from optimizer import substantiate as ss
+from classes import substantiate as ss
 from classes import proposed_stiffener as ps
 from classes import stiffeners_proposition as st_prop
 import initial_cs as ics
 from output import geometry_output as go
+from classes import stiffener as st
 
 #assign input to variables
 b_sup = 4000 #data.input_data["b_sup"]
@@ -23,15 +24,17 @@ prop_1 = ps.proposed_stiffener(3, 1, 0.5, 20*10**6)
 prop_2 = ps.proposed_stiffener(3, 2, -0.5, 10**7)
 prop_3 = ps.proposed_stiffener(2, 1, 0.5, 15*10**6)
 prop_4 = ps.proposed_stiffener(4, 1, 0.9, 10**6)
-prop_5 = ps.proposed_stiffener(3,1, 0, 3*10**6)
+prop_5 = ps.proposed_stiffener(3,1, -0.3, 3*10**6)
 
 #add all propositions to a list
 prop_list = st_prop.stiffeners_proposition()
 prop_list.add(prop_1)
-prop_list.add(prop_2)
-prop_list.add(prop_3)
-prop_list.add(prop_4)
-prop_list.add(prop_5)
+#prop_list.add(prop_2)
+#prop_list.add(prop_3)
+#prop_list.add(prop_4)
+#prop_list.add(prop_5)
 
-stiffener_list = ss.substantiate(test_cs, prop_list)
-go.print_cs(test_cs, stiffener_list)
+st.add_stiffener_set(test_cs, prop_list)
+
+#stiffener_list = ss.substantiate(test_cs, prop_list)
+#go.print_cs(test_cs, stiffener_list)
