842b1ad1cfd8bfb6f3ea28ae4becac0805bfbd4a
muellvin <78660277+muellvin@users.noreply.github.com>
2021-04-05 15:42:05 +0200

    outline buckling

--- proofs/buckling_proof.py
+++ proofs/buckling_proof.py
@@ -1,1 +1,36 @@
 #buckling proof
+
+
+
+"""3.2.1 mittragende Breite"""
+
+
+
+
+"""4 Längspannung"""
+
+"""4.4 Einzelfeldbeulen"""
+#in a first step, the effective width of the plates are determined (rho of each plate)
+#(iterative process): moving p1 and p2 of the
+
+"""4.5 Längsausgesteifte Blechfelder"""
+#plate behaviour with help of EB Plate
+#column behaviour with 4.5
+#interaction of plate and column in 4.5.4
+
+"""4.6 Nachweis"""
+#gives us eta 1
+
+
+"""5 Schubbeulen"""
+
+"""5.5 Nachweis"""
+#gives us eta 3
+
+
+"""6 Querbelastung"""
+#is already accounted for when dimensioning the deck??
+
+
+"""7 Interaktion"""
+#7.1 Interaction between shear, bending moment and normal force with eta 1 and eta 3
