28aab9b4954fef9f9d4280dc8e482eb32cbe15d0
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-04-12 09:08:18 +0200

    ebplate works :-)

--- ebplate/ebplate.py
+++ ebplate/ebplate.py
@@ -1,89 +1,85 @@
 import os
+import sys
 
 def ebplate(b, h, t, sigma_a, sigma_b, stiffeners_ebp):
     #write the input for ebplate to ebplate.EBP
-    input_file = open('ebplate\plate.EBP', 'w', encoding = 'latin-1')
-    preamble = ["EBPlate - v2.01 \n",
-                "Firm          # \n",\
-                "Contract      # \n",\
-                "Item          # \n",\
-                "Users         # \n",\
-                "Commentary    # \n"]
-    input_file.writelines(preamble)
-    plate = ["#PLATE \n",\
-             "   Width           " + str(b/10) + " cm \n",\
-             "   Height          " + str(h/10) + " cm \n",\
-             "   Thickness       " + str(t/10) + " cm \n",\
-             "   Young Modulus    210000.000 MPa \n",\
-             "   Poisson Ratio        0.300 \n",\
-             "   Boundary Conditions \n",\
-             "      Edge 1 0 \n",\
-             "      Edge 2 0 \n",\
-             "      Edge 3 0 \n",\
-             "      Edge 4 0 \n"]
-    input_file.writelines(plate)
-    stiffening_preamble = ["#STIFFENING \n",\
-                  "   Orthotropic Plate =  1 \n",\
-                  "   Orthotropic coefficients \n",\
-                  "   X Direction : Beta = 0 \n",\
-                  "                 Eta = -1 \n",\
-                  "   Smearing : No \n",\
-                  "   Y Direction : Beta = 0 \n",\
-                  "                 Eta = 0 \n",\
-                  "   Referential 1 \n",\
-                  "   Number of stiffeners = " + str(len(stiffeners_ebp)) + " \n"]
-    input_file.writelines(stiffening_preamble)
+    os.remove("ebplate\plate.EBP")
+    file = open("ebplate\plate.EBP", 'a+', encoding ='cp1252')
+    file.write("	EBPlate - v2.01 \n")
+    file.write("Société       # \n")
+    file.write("Affaire       # \n")
+    file.write("Référence     # \n")
+    file.write("Utilisateur   # \n")
+    file.write("Commentaire   # \n")
+    file.write("#PLATE \n")
+    file.write("   Width           " + str(b/10) + " cm \n")
+    file.write("   Height          " + str(h/10) + " cm \n")
+    file.write("   Thickness         " + str(t/10) + " cm \n")
+    file.write("   Module Young    210000.000 MPa \n")
+    file.write("   Poisson Coefficient         0.300 \n")
+    file.write("   Boundary Conditions \n")
+    file.write("      Bord 1 0 \n")
+    file.write("      Bord 2 0 \n")
+    file.write("      Bord 3 0 \n")
+    file.write("      Bord 4 0 \n")
+    file.write("#STIFFENING \n")
+    file.write("   Orthotropic Plate =  1 \n")
+    file.write("   Coefficients d'orthotropie : \n")
+    file.write("      Direction X : Beta =  0.000 \n")
+    file.write("                     Eta =  -1.000 \n")
+    file.write("        Smearing : No \n")
+    file.write("      Direction Y : Beta =  0.000 \n")
+    file.write("                     Eta =  0.000 \n")
+    file.write("        Smearing : No \n")
+    file.write("   Referentiel 2 \n")
+    file.write("   Nombre de raidisseurs  " + str(len(stiffeners_ebp)) + " \n")
     for i in range(len(stiffeners_ebp)):
-        stiffening = ["    - Stiffener n° " + str(i+1) + " Active=1 \n",\
-                      "      Orientation = 0 \n",\
-                      "      Location    = " + str(stiffeners_ebp[i][0]/10) + " \n",\
-                      "      Gamma       = " + str(stiffeners_ebp[i][1]) + " \n",\
-                      "      Teta        = " + str(stiffeners_ebp[i][2]) + " \n",\
-                      "      Delta       = " + str(stiffeners_ebp[i][3]) + " \n",\
-                      "      Type        =  0 \n",\
-                      "      Dimension 1 = 0 \n",\
-                      "      Dimension 2 = 0 \n",\
-                      "      Dimension 3 = 0 \n",\
-                      "      Dimension 4 = 0 \n"]
-        input_file.writelines(stiffening)
-    stresses = ["#STRESSES \n",\
-                "Longitudinal stresses : User's data No \n",\
-                 "Analytical Longitudinal stresses \n",\
-                  "Imposed values : No \n",\
-                   "Longitudinal Stress Top Left                " + str(sigma_a) + " MPa \n",\
-                   "Longitudinal Stress Bottom Left             " + str(sigma_b) + " MPa \n",\
-                   "Longitudinal Stress Top Right               " + str(sigma_a) + " MPa \n",\
-                   "Longitudinal Stress Bottom Right            " + str(sigma_b) + " MPa \n",\
-                 "Transverse stresses : User's data No \n",\
-                 "Analytical Transverse stresses \n",\
-                  "Imposed values : No \n",\
-                   "Transverse stress Top                         0.000 MPa \n",\
-                   "Transverse stress Bottom                      0.000 MPa \n",\
-                   "Patch Load Stress Top                         0.000 MPa \n",\
-                   "Patch Load Stress Bottom                      0.000 MPa \n",\
-                   "Patch Load Width Top                          0.000 cm \n",\
-                   "Patch Load Width Bottom                       0.000 cm \n",\
-                   "Local longitudinal stresses under patch load : No \n",\
-                   "Local shear stresses under patch load : Yes \n",\
-                   "W Top                      -1.000 \n",\
-                   "W Bot                      -1.000 \n",\
-                   "Shear distribution  1 \n",\
-                 "Shear stresses : User's data No \n",\
-                 "Analytical Shear stresses \n",\
-                  "Imposed values : No \n",\
-                   "Shear stress                                 0.000 MPa \n"]
-    input_file.writelines(stresses)
-    parameters = ["#PARAMETERS \n",\
-                   "Complexity : 1 \n",\
-                   "Effective width parameter :  10.000"]
-    input_file.writelines(parameters)
-    input_file.close()
-
+        file.write("   - Raidisseur n° " + str(i+1) + " Active=1 \n")
+        file.write("      Orientation = 0 \n")
+        file.write("      Position    = " + str(stiffeners_ebp[i][0]/10) + " \n")
+        file.write("      Gamma       = " + str(stiffeners_ebp[i][1]) + " \n")
+        file.write("      Teta        = " + str(stiffeners_ebp[i][2]) + " \n")
+        file.write("      Delta       = " + str(stiffeners_ebp[i][3]) + " \n")
+        file.write("      Dimension 1 = 0 \n")
+        file.write("      Type        = 0 \n")
+        file.write("      Dimension 2 = 0 \n")
+        file.write("      Dimension 3 = 0 \n")
+        file.write("      Dimension 4 = 0 \n")
+    file.write("#STRESSES \n")
+    file.write(" Longitudinal stresses : User's data No \n")
+    file.write(" Analytical Longitudinal stresses \n")
+    file.write("  Imposed values : No \n")
+    file.write("   Longitudinal Stress Top Left                " + str(sigma_a) + " MPa \n")
+    file.write("   Longitudinal Stress Bottom Left             " + str(sigma_b) + " MPa \n")
+    file.write("   Longitudinal Stress Top Right               " + str(sigma_a) + " MPa \n")
+    file.write("   Longitudinal Stress Bottom Right            " + str(sigma_b) + " MPa \n")
+    file.write(" Transverse stresses : User's data No \n")
+    file.write(" Analytical Transverse stresses \n")
+    file.write("  Imposed values : No \n")
+    file.write("   Transverse stress Top                         0.000 MPa \n")
+    file.write("   Transverse stress Bottom                      0.000 MPa \n")
+    file.write("   Patch Load Stress Bottom                      0.000 MPa \n")
+    file.write("   Patch Load Stress Top                         0.000 MPa \n")
+    file.write("   Patch Load Width Top                          0.000 cm \n")
+    file.write("   Patch Load Width Bottom                       0.000 cm \n")
+    file.write("   Local longitudinal stresses under patch load : No \n")
+    file.write("   Local shear stresses under patch load : Yes \n")
+    file.write("   W Top                      -1.000 \n")
+    file.write("   W Bot                      -1.000 \n")
+    file.write("   Shear distribution  1 \n")
+    file.write("Shear stresses : User's data No \n")
+    file.write("Analytical Shear stresses \n")
+    file.write("  Imposed values : No \n")
+    file.write("   Shear stress                                 0.000 MPa \n")
+    file.write("#PARAMETERS \n")
+    file.write("   Complexity : 1 \n")
+    file.write("   Effective width parameter :  10.000")
+    file.close()
     #run ebplate calculation from your own directory
     os.system(r'"C:\Program Files (x86)\EBPlate\EBPlate.exe" /BATCH C:\Users\Nino\Google Drive\Studium\FS 2021\Bachelorarbeit\BA\ebplate\plate.EBP')
 
     #get phi_cr from the ebplate.EBR
-    result_file = open('ebplate\plate.EBR', 'r', encoding = 'latin-1')
+    result_file = open('ebplate\plate.EBR', 'r', encoding = 'cp1252')
     results_text = result_file.readlines()
     phi_cr_line = results_text[1]
     phi_cr = float(phi_cr_line[7:])
--- tests/test_nino.py
+++ tests/test_nino.py
@@ -7,8 +7,8 @@ sys.path.append('C:/Users/Nino/Google Drive/Studium/FS 2021/Bachelorarbeit/BA/eb
 
 import ebplate as ebp
 stiffeners_ebp = []
-stiffeners_ebp.append((600, 10, 6, 0.2))
-#stiffeners_ebp.append((1200, 10, 6, 0.2))
+stiffeners_ebp.append((500, 10, 6, 0.2))
+stiffeners_ebp.append((1050, 10, 6, 0.2))
 phi_cr = ebp.ebplate(3000,2000,20,200,-200, stiffeners_ebp)
 print(phi_cr)
 #test
