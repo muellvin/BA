bb0e36fd3a15ca81970352c9c630074fe094d51e
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-04-13 08:45:58 +0200

    debugging

--- proofs/global_buckling.py
+++ proofs/global_buckling.py
@@ -1,32 +1,42 @@
 import math
 import defaults
-import stresscal as stc
+from proofs import stress_cal as stc
 from ebplate import ebplate as ebp
-import line as ln
+from classes import line as ln
+from classes import crosssection as cs
+from classes import point as pt
+from output import geometry_output as go
+import random
+import data
 
-def global_plate_buckling(cs, stiffened_plate):
+def global_plate_buckling(total_cs, stiffened_plate):
     #all methods still to be correctly implemented
-
     #identify the border plates a and b and numbers of end stiffeners
     plate_a = None
     plate_b = None
-    max_tpl = random.choice(stiffened_plate.lines).code.tpl_number
-    min_tpl = random.choice(stiffened_plate.lines).code.tpl_number
+    min_tpl = -1
+    max_tpl = -1
     max_stn = 0
     min_stn = 1000
 
     for plate in stiffened_plate.lines:
-        if plate.code.tpl_number <= min_tpl:
-            plate_a = plate
-        if plate.code.tpl_number >= max_tpl:
-            plate_b = plate
-        if plate.code.st_number <= min_stn and plate.code.st_number != 0:
+        if plate.code.pl_type == 0:
+            if min_tpl == -1 and max_tpl == -1:
+                min_tpl = plate.code.tpl_number
+                max_tpl = plate.code.tpl_number
+            if plate.code.tpl_number <= min_tpl:
+                plate_a = plate
+            if plate.code.tpl_number >= max_tpl:
+                plate_b = plate
+        elif plate.code.st_number <= min_stn and plate.code.st_number != 0:
             min_stn = plate.code.st_number
-        if plate.code.st_number >= max_stn and plate.code.st_number != 0:
+        elif plate.code.st_number >= max_stn and plate.code.st_number != 0:
             max_stn = plate.code.st_number
 
-    sigma_a = stc.get_sigma_a(cs, plate_a, data.input_data.get("M_Ed"))
-    sigma_b = stc.get_sigma_b(cs, plate_b, data.input_data.get("M_Ed"))
+    assert plate_a != None and plate_b != None, "For-Loop failed."
+
+    sigma_a = stc.get_sigma_a(total_cs, plate_a, data.input_data.get("M_Ed"))
+    sigma_b = stc.get_sigma_b(total_cs, plate_b, data.input_data.get("M_Ed"))
     psi = min(sigma_a, sigma_b) / max(sigma_a, sigma_b)
 
     #get plate geometry
@@ -34,16 +44,25 @@ def global_plate_buckling(cs, stiffened_plate):
     for plate in stiffened_plate.lines:
         if plate.code.pl_type == 0:
             h += plate.get_length_tot()
+            print(plate.get_length_tot())
     t = plate_a.t
+    print("t = " + str(t))
     b = defaults.plate_length
 
+    go.print_cs(stiffened_plate)
     #coordinate transformation (a+b)/2 is the new origin and baseplate is horizontal
     #set new origin
+    print("Point a:  " + str(plate_a.a))
+    print("Point b:  " + str(plate_b.b))
+    move_z = 0.5*(plate_a.a.z + plate_b.b.z)
+    move_y = 0.5*(plate_a.a.y + plate_b.b.y)
+
+    "This part urgently needs to be changed because it is very faulty."
     for plate in stiffened_plate.lines:
-        plate.a.y -= 0.5*(plate_a.a.y + plate_b.b.y)
-        plate.a.z -= 0.5*(plate_a.a.z + plate_b.b.z)
-        plate.b.y -= 0.5*(plate_a.a.y + plate_b.b.y)
-        plate.b.z -= 0.5*(plate_a.a.z + plate_b.b.z)
+        plate.a.y -= move_y
+        plate.a.z -= move_z
+        plate.b.y -= move_y
+        plate.b.z -= move_z
 
     #find rotation angle
     if plate_a.a.z == plate_b.b.z:
@@ -54,8 +73,9 @@ def global_plate_buckling(cs, stiffened_plate):
         angle = math.pi/2
     else:
         #inclined plate
-        angle = - math.atan((plate.b.z-plate.a.z)/(plate.b.y-plate.a.y))
+        angle = - math.atan((plate_b.b.z-plate_a.a.z)/(plate_b.b.y-plate_a.a.y))
 
+    print("Angle = " + str(angle))
     #perform rotation
     for plate in stiffened_plate.lines:
         ay = plate.a.y
@@ -73,16 +93,21 @@ def global_plate_buckling(cs, stiffened_plate):
     #create a list of all stiffeners
     stiffener_list = []
     plate_num_list = []
+    corresp_tpl = -2
     for st_number in range(min_stn, max_stn+1, 1):
-        stiffener_list[st_number - min_stn] = cs.crosssection()
-        corresp_tpl = 0
-        for plate in stiffened_plate:
+        stiffener_list.insert(st_number - min_stn, cs.crosssection(0,0,0))
+        #find three plates of stiffener
+        for plate in stiffened_plate.lines:
             if plate.code.st_number == st_number:
                 stiffener_list[st_number - min_stn].addline(plate)
-                if plate.code_tpl_number =! 0:
-                    corresp_tpl = plate.code_tpl_number
-                    plate_num_list[st_number - min_stn] = corresp_tpl
-        for plate in stiffened_plate:
+                #find middle part of the trapezoid
+                if plate.code.tpl_number != 0:
+                    corresp_tpl = plate.code.tpl_number
+                    plate_num_list.insert(st_number - min_stn, corresp_tpl)
+                else:
+                    corresp_tpl = -2
+        #find adjacent parts of the trapezoid
+        for plate in stiffened_plate.lines:
             if plate.code.tpl_number == corresp_tpl -1 or plate.code.tpl_number == corresp_tpl +1:
                 stiffener_list[st_number - min_stn].addline(plate)
 
@@ -96,24 +121,26 @@ def global_plate_buckling(cs, stiffened_plate):
         center_plate = None
         top_plate = None
         bottom_plate = None
-        for plate in stiffener.lines:
-            if plate.code.tpl_number == corresp_tpl:
+        for plate in stiffened_plate.lines:
+            if plate.code.tpl_number == corresp_tpl and plate.code.pl_type == 0:
                 center_plate = plate
-            elif plate.code.tpl_number == corresp_tpl+1:
+            if plate.code.tpl_number == corresp_tpl+1:
                 bottom_plate = plate
             elif plate.code.tpl_number == corresp_tpl-1:
                 top_plate = plate
         #find distance to point a
-        delta_z = center_plate.cal_center_z() - plate_a.a.z
-        delta_y = center_plate.cal_center_y() - plate_y.a.y
+        delta_z = center_plate.get_center_z_tot() - plate_a.a.z
+        print("Delta_z :" +str(delta_z))
+        delta_y = center_plate.get_center_y_tot() - plate_a.a.y
+        print("Delta_y :" +str(delta_y))
         distance = math.sqrt(delta_z **2 + delta_y **2)
 
         #calculate delta
         unit_vec_to_a = (top_plate.a.y - top_plate.b.y) / top_plate.get_length_tot()
-        unit_vec_to_b = (bottom_plate.b.y - bottom_plate.a.y) / top_plate.get_length_tot()
+        unit_vec_to_b = (bottom_plate.b.y - bottom_plate.a.y) / bottom_plate.get_length_tot()
         #find additional parts of top plate
-        sigma_a_top = stc.get_sigma_a(cs, top_plate, data.input_data.get("M_Ed"))
-        sigma_b_top = stc.get_sigma_b(cs, top_plate, data.input_data.get("M_Ed"))
+        sigma_a_top = stc.get_sigma_a(total_cs, top_plate, data.input_data.get("M_Ed"))
+        sigma_b_top = stc.get_sigma_b(total_cs, top_plate, data.input_data.get("M_Ed"))
         psi_top =  min(sigma_a_top, sigma_b_top) / max(sigma_a_top, sigma_b_top)
         length_top = 0
         if sigma_a_top > 0 or sigma_b_top > 0:
@@ -138,38 +165,36 @@ def global_plate_buckling(cs, stiffened_plate):
             pass
 
         #find additional parts of bottom plate
-        sigma_a_bottom = stc.get_sigma_a(cs, top_plate, data.input_data.get("M_Ed"))
-        sigma_b_bottom = stc.get_sigma_b(cs, top_plate, data.input_data.get("M_Ed"))
+        sigma_a_bottom = stc.get_sigma_a(total_cs, top_plate, data.input_data.get("M_Ed"))
+        sigma_b_bottom = stc.get_sigma_b(total_cs, top_plate, data.input_data.get("M_Ed"))
         psi_bottom = min(sigma_a_bottom, sigma_b_bottom) / max(sigma_a_bottom, sigma_b_bottom)
         length_bottom = 0
-                if sigma_a_bottom > 0 or sigma_b_bottom > 0:
-                    #stiffener is at least partly in compression zone
-                    if sigma_b_botton > sigma_a_bottom:
-                        if psi_bottom > 0:
-                            #low stress side
-                            length_bottom = bottom_plate.get_length_tot()*(3-psi_bottom)/(5-psi_bottom)
-                        else:
-                            #tension zone
-                            pass
-                    else:
-                        if psi_bottom > 0:
-                            #high stress side
-                            length_bottom = bottom_plate.get_length_tot()*2/(5-psi_bottom)
-                        else:
-                            b_comp = bottom_plate.get_length_tot()/(1-psi_bottom)
-                            length_bottom = 0.4*b_comp
-                            pass
+        if sigma_a_bottom > 0 or sigma_b_bottom > 0:
+            #stiffener is at least partly in compression zone
+            if sigma_b_bottom > sigma_a_bottom:
+                if psi_bottom > 0:
+                    #low stress side
+                    length_bottom = bottom_plate.get_length_tot()*(3-psi_bottom)/(5-psi_bottom)
                 else:
-                    #stiffener is in tension zone
+                    #tension zone
                     pass
+            else:
+                if psi_bottom > 0:
+                    #high stress side
+                    length_bottom = bottom_plate.get_length_tot()*2/(5-psi_bottom)
+                else:
+                    b_comp = bottom_plate.get_length_tot()/(1-psi_bottom)
+                    length_bottom = 0.4*b_comp
+                    pass
+        else:
+            #stiffener is in tension zone
+            pass
 
         #add additional parts of plate to stiffener
-        new_top_plate = ln.line(top_plate.code, top_plate.b + unit_vec_to_a * length_top, \
-        top_plate.b, top_plate.t)
-        new_bottom_plate = ln.line(bottom_plate.code, bottom_plate.a, bottom_plate.a + \
-        unit_vec_to_b * length_bottom, top_plate.t)
-        stiffener.lines.remove(top_plate)
-        stiffener.lines.remove(bottom_plate)
+        new_top_point = pt.point(top_plate.b.y + unit_vec_to_a * length_top, top_plate.b.z)
+        new_top_plate = ln.line(top_plate.code, new_top_point, top_plate.b, top_plate.t)
+        new_bottom_point = pt.point(bottom_plate.a.y + unit_vec_to_b * length_bottom, bottom_plate.a.z)
+        new_bottom_plate = ln.line(bottom_plate.code, bottom_plate.a, new_bottom_point, top_plate.t)
         stiffener.lines.append(new_top_plate)
         stiffener.lines.append(new_bottom_plate)
 
@@ -179,44 +204,46 @@ def global_plate_buckling(cs, stiffened_plate):
         #calculate gamma
         length_gamma = defaults.effective_width_parameter*center_plate.t
         if length_gamma > 0.5*center_plate.get_length_tot():
-            new_top_plate.a = new_top_plate.b + unit_vec_to_a*length_gamma
-            new_bottom_plate.b = new_bottoom_plate.a + unit_vec_to_b*length_gamma
+            new_top_plate.a = pt.point(new_top_plate.b.y + unit_vec_to_a*length_gamma, new_top_plate.b.z)
+            new_bottom_plate.b = pt.point(new_bottom_plate.a.y + unit_vec_to_b*length_gamma, new_bottom_plate.a.z)
         else:
             stiffener.lines.remove(center_plate)
-            new_top_plate.a = new_top_plate.b + unit_vec_to_a*length_gamma
-            new_top_plate.b = new_top_plate.b + unit_vec_to_b*length_gamma
-            new_bottom_plate.b = new_bottoom_plate.a + unit_vec_to_b*length_gamma
-            new_bottom_plate.a = new_bottoom_plate.a + unit_vec_to_a*length_gamma
+            new_top_plate.a = pt.point(new_top_plate.b.y + unit_vec_to_a*length_gamma, new_top_plate.b.z)
+            new_top_plate.b = pt.point(new_top_plate.b.y + unit_vec_to_b*length_gamma, new_top_plate.b.z)
+            new_bottom_plate.b = pt.point(new_bottom_plate.a.y + unit_vec_to_b*length_gamma, new_bottom_plate.a.z)
+            new_bottom_plate.a = pt.point(new_bottom_plate.a.y + unit_vec_to_a*length_gamma, new_bottom_plate.a.z)
 
         gamma = stiffener.get_i_y_tot() * 12 * (1-0.3**2)/(h*t**3)
 
         #calculate theta
         #tbd
-        theta = 0
-        stiffeners_ebp[counter] = (distance, gamma, theta, delta)
+        theta = 1.0
+        stiffeners_ebp.insert(i,(distance, gamma, theta, delta))
 
     #finding critical buckling load
     phi_cr_p = ebp.ebplate(b,h,t,sigma_a, sigma_b, stiffeners_ebp)
     sigma_max = max(sigma_a, sigma_b)
     sigma_cr_p = sigma_max * phi_cr_p
+    print("sigma_cr = " + str(sigma_cr_p))
 
     #calculating plate slenderness
     "correct beta_a_c still to be implemented"
     beta_a_c = 1
-    lambda_p_glob_bar = math.sqrt(beta_a_c * data.constants.get("fy") / sigma_cr_p)
-
+    lambda_p_glob_bar = math.sqrt(beta_a_c * data.constants.get("f_y") / sigma_cr_p)
+    print("Lambda: " + str(lambda_p_glob_bar))
     #calculate rho_glob for plate buckling
-    #assumption: cs parts always supported on both sides
+    #assumption: cross section parts always supported on both sides
     rho_glob = 0
     #held on both sides
-    if lamdbda_p_glob_bar <= 0.673:
+    if lambda_p_glob_bar <= 0.673:
         rho_glob = 1.0
     elif lambda_p_glob_bar > 0.673 and (3 + psi) >= 0:
-        rho_glob = (lambda_p_loc_bar - 0.055 * (3 + psi)) / lambda_p_glob_bar**2
-        if rho_glob > 1.0
+        rho_glob = (lambda_p_glob_bar - 0.055 * (3 + psi)) / lambda_p_glob_bar**2
+        if rho_glob > 1.0:
             rho_glob = 1.0
     else:
         print("plate slenderness or stress ratio out of range")
         pass
 
+    print("Rho_Global: " + str(rho_glob))
     return rho_glob
--- tests/test_ebplate.py
+++ tests/test_ebplate.py
@@ -1,15 +1,37 @@
 import sys
 import os
+
 #just add the directory where the BA folder is on your computer
 sys.path.append('C:/Users/Nino/Google Drive/Studium/FS 2021/Bachelorarbeit/BA')
-sys.path.append('C:/Users/Nino/Google Drive/Studium/FS 2021/Bachelorarbeit/BA/ebplate')
 
-os.system(r'"C:\Program Files (x86)\EBPlate\EBPlate.exe" /BATCH C:\Users\Nino\Google Drive\Studium\FS 2021\Bachelorarbeit\BA\ebplate\plate-original.EBP')
+import initial_cs as ics
+from classes import stiffener as st
+from output import geometry_output as go
+from classes import point as pt
+from classes import line as ln
+from classes import plate_code as plcd
+from classes import crosssection as cs
+from proofs import global_buckling as glb
+import data
+import math
 
-#get phi_cr from the ebplate.EBR
-result_file = open('ebplate\plate-original.EBR', 'r')
-results_text = result_file.readlines()
-phi_cr_line = results_text[1]
-phi_cr = float(phi_cr_line[7:])
+data.input_data.update({"M_Ed": -45000*10**6})
 
-print(phi_cr)
+initial_cs = ics.create_initial_cs(4000, 3000, 2000, 10, 10, 5)
+stiffener_1 = st.create_stiffener_global(3, 1, -1000, 2000, math.pi, 200, 150, 100, 5)
+stiffener_2 = st.create_stiffener_global(3, 2, 1000, 2000, math.pi, 200, 150, 100, 5)
+
+
+stiffener_list = [stiffener_1, stiffener_2]
+final_cs = st.merge(initial_cs, stiffener_list)
+final_cs = initial_cs
+
+point_a = pt.point(-1500, 2000)
+point_b = pt.point(1500, 2000)
+code = plcd.plate_code(3,0,3,0,0)
+plate = ln.line(code, point_a, point_b, 5)
+x_sec = cs.crosssection(4000, 3000, 2000)
+x_sec.lines.append(plate)
+stiffened_plate = st.merge(x_sec, stiffener_list)
+
+glb.global_plate_buckling(final_cs, stiffened_plate)
