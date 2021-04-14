4126594765bccc84ad8eb13905187e907a946b5a
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-04-14 22:30:03 +0200

    debugging merge

--- tests/merge_test.py
+++ tests/merge_test.py
@@ -5,17 +5,17 @@ import os
 sys.path.append('C:/Users/Nino/Google Drive/Studium/FS 2021/Bachelorarbeit/BA')
 
 import initial_cs as ics
+import math 
 from classes import stiffener as st
 from output import geometry_output as go
 from classes import point as pt
 from classes import line as ln
 from classes import plate_code as plcd
 from classes import crosssection as cs
-from proofs import global_buckling as glb
 
 initial_cs = ics.create_initial_cs(4000, 3000, 2000, 20, 20, 20)
-stiffener_1 = st.create_stiffener_global(3, 1, -1000, 2000, 0, 300, 200, 200, 15)
-stiffener_2 = st.create_stiffener_global(3, 2, 1000, 2000, 0, 300, 200, 200, 15)
+stiffener_1 = st.create_stiffener_global(3, 1, -1000, 2000, math.pi, 300, 200, 200, 15)
+stiffener_2 = st.create_stiffener_global(3, 2, 1000, 2000, math.pi, 300, 200, 200, 15)
 
 
 stiffener_list = [stiffener_1, stiffener_2]
