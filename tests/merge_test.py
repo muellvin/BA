9f00483f7a500aa63743cdaf7c22b824417cad90
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-04-16 10:45:01 +0200

    test cases for merge

--- tests/merge_test.py
+++ tests/merge_test.py
@@ -15,14 +15,14 @@ from classes import crosssection as cs
 import deck
 
 initial_cs = ics.create_initial_cs(4000, 3000, 2000, 20, 20, 20)
-#stiffener_1 = st.create_stiffener_global(1, 1, 1000, 0, 0, 300, 200, 200, 15)
+stiffener_1 = st.create_stiffener_global(1, 1, 1000, 0, 0, 300, 200, 200, 15)
 #stiffener_2 = st.create_stiffener_global(1, 2, -1000, 0, 0, 300, 200, 200, 15)
 #stiffener_3 = st.create_stiffener_global(2, 3, -2000, 1000, 3*math.pi/2, 200, 100, 100, 10)
 #stiffener_4 = st.create_stiffener_global(3, 4, -1000, 2000, math.pi, 300, 200, 200, 15)
 #stiffener_5 = st.create_stiffener_global(3, 5, 1000, 2000, math.pi, 300, 200, 200, 15)
 #stiffener_6 = st.create_stiffener_global(4, 6, 2000, 1000, math.pi/2, 200, 100, 100, 10)
 
-stiffener_list = []
+stiffener_list = [stiffener_1]
 #deck.deck(4000)
 final_cs = st.merge(initial_cs, stiffener_list)
 for line in final_cs.lines:
--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,7 +1,8 @@
 import unittest
 from initial_cs import create_initial_cs
 from classes import stiffener as st
-
+from classes import plate_code as plcd
+from classes import crosssection
 
 class TestMerge(unittest.TestCase):
 
@@ -10,7 +11,26 @@ class TestMerge(unittest.TestCase):
         stiffeners = []
         final_cs = st.merge(initial_cs, stiffeners)
         length = len(final_cs.lines)
+        line_4 = final_cs.get_line(pl_type = 0, pl_position = 4, tpl_number = 4)
+        code_4 = plcd.plate_code(4,0,4,0,0)
         self.assertEqual(length, 4)
+        self.assertEqual(line_4.code, code_4)
+
+    def test_stiffeners_top(self):
+        initial_cs = create_initial_cs(4000, 3000, 2000, 20, 20, 20)
+        stiffener_1 = st.create_stiffener_global(1, 1, 1000, 0, 0, 300, 200, 200, 15)
+        stiffener_2 = st.create_stiffener_global(1, 2, -1000, 0, 0, 300, 200, 200, 15)
+        stiffeners = [stiffener_1, stiffener_2]
+        final_cs = st.merge(initial_cs, stiffeners)
+        length = len(final_cs.lines)
+        self.assertEqual(length, 14)
+        stiffener_1_line_3 = final_cs.get_line(st_number = 1, st_pl_position = 3)
+        st_1_3_length = stiffener_1_line_3.get_length_tot()
+        self.assertEqual(st_1_3_length, 200)
+        line_top_right = final_cs.get_line(tpl_number = 5)
+        code_5 = plcd.plate_code(1,0,5,0,0)
+        self.assertEqual(line_top_right.code, code_5)
+        self.assertEqual(line_top_right.a.y, -1150)
 
 if __name__ == '__main__':
     unittest.main()
