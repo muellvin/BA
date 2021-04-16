704b3e34852cd6be9b1c03c9ac6b5bb027e043d9
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-04-16 09:45:18 +0200

    merge

--- tests/merge_test.py
+++ tests/merge_test.py
@@ -12,16 +12,20 @@ from classes import point as pt
 from classes import line as ln
 from classes import plate_code as plcd
 from classes import crosssection as cs
+import deck
 
 initial_cs = ics.create_initial_cs(4000, 3000, 2000, 20, 20, 20)
-stiffener_1 = st.create_stiffener_global(1, 1, 1000, 0, 0, 300, 200, 200, 15)
-stiffener_2 = st.create_stiffener_global(1, 2, -1000, 0, 0, 300, 200, 200, 15)
-stiffener_3 = st.create_stiffener_global(3, 3, -1000, math.pi, 0, 300, 200, 200, 15)
-stiffener_4 = st.create_stiffener_global(3, 4, 1000, math.pi, 0, 300, 200, 200, 15)
+#stiffener_1 = st.create_stiffener_global(1, 1, 1000, 0, 0, 300, 200, 200, 15)
+#stiffener_2 = st.create_stiffener_global(1, 2, -1000, 0, 0, 300, 200, 200, 15)
+#stiffener_3 = st.create_stiffener_global(2, 3, -2000, 1000, 3*math.pi/2, 200, 100, 100, 10)
+#stiffener_4 = st.create_stiffener_global(3, 4, -1000, 2000, math.pi, 300, 200, 200, 15)
+#stiffener_5 = st.create_stiffener_global(3, 5, 1000, 2000, math.pi, 300, 200, 200, 15)
+#stiffener_6 = st.create_stiffener_global(4, 6, 2000, 1000, math.pi/2, 200, 100, 100, 10)
 
-stiffener_list = [stiffener_1, stiffener_2, stiffener_3, stiffener_4]
+stiffener_list = []
+#deck.deck(4000)
 final_cs = st.merge(initial_cs, stiffener_list)
 for line in final_cs.lines:
     print("Line:  " + str(line.a) + " " + str(line.b))
     print(str(line.code))
-go.print_cs(final_cs)
+go.print_cs_red(final_cs)
---
+++ tests/test_merge.py
@@ -0,0 +1,19 @@
+import unittest
+from initial_cs import create_initial_cs
+from classes import stiffener as st
+
+
+class TestMerge(unittest.TestCase):
+
+    def test_empty(self):
+        initial_cs = ics.create_initial_cs(4000, 3000, 2000, 20, 20, 20)
+        stiffeners = []
+        final_cs = st.merge(initial_cs, stiffeners)
+        length = len(final_cs)
+        final_cs.get_line()
+        self.assertEqual(length, 4)
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()
