6806a70edbb3c9f531d1f113ed400589619045ee
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-04-19 09:24:51 +0200

    debugging

--- proofs/global_buckling.py
+++ proofs/global_buckling.py
@@ -3,9 +3,11 @@ import defaults
 from proofs import stress_cal as stc
 from ebplate import ebplate as ebp
 from classes import line as ln
-from classes import crosssection as cs
+from classes import crosssection
 from classes import point as pt
 from output import geometry_output as go
+from proofs import column_buckling as column
+from proofs import global_plate_buckling as plate_global
 import random
 import data
 
@@ -17,17 +19,20 @@ def global_buckling(cs):
 
 #create a cs with all plates of this side
 def reduction_global_buckling(cs, side):
-    plate_glob = crosssection.crosssection()
+    plate_glob = crosssection.crosssection(0,0,0)
     for line in cs.lines:
-    if line.code.pl_position == side:
-        plate_glob.addline(line)
+        if line.code.pl_position == side:
+            plate_glob.addline(line)
 
-    chi_c, sigma_cr_c = column.column(plate_glob, side)
-    rho_p, sigma_cr_p = plate_global.plate_global(cs, plate_glob)
+    chi_c, sigma_cr_c = column.column_buckling(plate_glob, side)
+    rho_p, sigma_cr_p = plate_global.global_plate_buckling(cs, plate_glob)
+    print("rho_p = " + str(rho_p))
+    print("chi_c = " + str(chi_c))
 
 
     eta = sigma_cr_p/sigma_cr_c -1
     rho_c = (rho_p - chi_c) * eta * (2 - eta) + chi_c
+    print("rho_c = " + str(rho_c))
 
     for line in cs.lines:
         if line.code.pl_position == side:
--- tests/nino_test.py
+++ tests/nino_test.py
@@ -5,11 +5,27 @@ import sys
 sys.path.append('C:/Users/Nino/Google Drive/Studium/FS 2021/Bachelorarbeit/BA')
 sys.path.append('C:/Users/Nino/Google Drive/Studium/FS 2021/Bachelorarbeit/BA/ebplate')
 
-import ebplate as ebp
-stiffeners_ebp = []
-stiffeners_ebp.append((500, 10, 6, 0.2))
-stiffeners_ebp.append((1050, 10, 6, 0.2))
-phi_cr = ebp.ebplate(3000,2000,20,200,-200, stiffeners_ebp)
-print(phi_cr)
+import initial_cs as ics
+from proofs import global_buckling as globb
+from proofs import local_buckling as locb
+from output import geometry_output as go
+import data
+
+data.input_data.update({"b_inf": 3000})
+data.input_data.update({"b_sup": 4000})
+data.input_data.update({"h": 2000})
+data.input_data.update({"M_Ed": 2*10**9})
+data.input_data.update({"Q_Ed": 1000})
+data.input_data.update({"T_Ed": 1000})
+data.input_data.update({"a": 1000})
+data.input_data.update({"L_e": 1000})
+data.input_data.update({"bending type": 1000})
+data.input_data.update({"cs position": 1000})
+initial_cs = ics.create_initial_cs(4000, 3000, 2000, 6, 6, 6)
+go.print_cs(initial_cs)
+reduced_cs = locb.local_buckling(initial_cs)
+go.print_cs_red(reduced_cs)
+final_cs = globb.global_buckling(reduced_cs)
+go.print_cs(final_cs)
 #test
 #test comment
