e458f1d8d5ab37bb2d47a84c1d2bb843d37ca049
muellvin <78660277+muellvin@users.noreply.github.com>
2021-05-21 15:04:44 +0200

    a

--- cs_analysis_tool/cs_analysis_tool_terminal.py
+++ cs_analysis_tool/cs_analysis_tool_terminal.py
@@ -1,20 +1,19 @@
-#script for analysing an existing crosssection
-from classes import stiffener
-from classes import crosssection
-from classes import substantiate
-from input import input_analysis_tool
-from output import printing
-from proofs import buckling_proof
-from classes import merge
-import initial_cs
-import data
-import deck
-from output import geometry_output
 import math
 import os
 import sys
 import defaults
-from optimizer import optimization_value
+from assembly import stiffener
+from assembly import substantiate
+from classes import crosssection
+from assembly import merge
+from cs_analysis_tool import cs_analysis_tool_terminal_input
+from user_interface/output import printing
+from user_interface/output import geometry_output
+from proofs_and_stress_calculation import buckling_proof
+from deck_and_initial_cs import initial_cs
+from deck_and_initial_cs import deck
+from data_and_defaults import data
+from cs_optimization_tool import optimization_value
 
 #sys.path.append('C:/Users/Vinzenz Müller/Dropbox/ETH/6. Semester/BA')
 #crosssection input and creation (only trapezoid plates)
@@ -23,10 +22,10 @@ if defaults.do_print_to_txt == True:
     file = open("output/cs_analysis.txt", "w+")
     file.close()
 defaults.do_deck_as_prop = True
-input_analysis_tool.set_defaults()
+cs_analysis_tool_terminal_input.set_defaults()
 printing.printing(data.constants_tostring(), terminal = True)
 
-input_analysis_tool.set_cs_geometry()
+cs_analysis_tool_terminal_input.set_cs_geometry()
 #data.check_input_data_complete()
 cs = initial_cs.create_initial_cs(data.input_data.get("b_sup"), data.input_data.get("b_inf"), data.input_data.get("h"), data.input_data.get("t_side"), data.input_data.get("t_deck"), data.input_data.get("t_bottom"))
 
@@ -42,7 +41,7 @@ st_list_deck = substantiate.substantiate(cs, st_list_deck)
 
 
 #add all other stiffeners
-input_analysis_tool.set_stiffeners(number_st_top)
+cs_analysis_tool_terminal_input.set_stiffeners(number_st_top)
 st_list_rest = []
 for st in data.stiffener_data.stiffeners:
     y,z = cs.get_coordinates(st.location, st.pl_position)
@@ -63,7 +62,7 @@ geometry_output.print_cs_red(cs)
 
 
 #set the cross-sectional forces
-input_analysis_tool.set_forces()
+cs_analysis_tool_terminal_input.set_forces()
 
 
 #buckling proof
