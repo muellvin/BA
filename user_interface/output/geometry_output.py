64f9841ad8c9011cca54338d53a231e73dd7e1cf
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-05-23 14:43:26 +0200

    debugging pdf creation

--- user_interface/output/geometry_output.py
+++ user_interface/output/geometry_output.py
@@ -132,4 +132,3 @@ def print_cs_to_png(crosssection, name, input = True, location = None):
             figure.savefig(location+name+"_in.png")
         else:
             figure.savefig(location+name+"_out.png")
-    plt.close(figure)
