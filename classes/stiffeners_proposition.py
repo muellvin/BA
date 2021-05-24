4845052794f21b081bb892198ce4f3d639bf3d25
muellvin <78660277+muellvin@users.noreply.github.com>
2021-05-24 17:03:19 +0200

    a

--- classes/stiffeners_proposition.py
+++ classes/stiffeners_proposition.py
@@ -1,27 +1,17 @@
 from data_and_defaults import defaults
 
 class stiffeners_proposition():
-
+    #constructor
     def __init__(self):
         self.stiffeners = []
 
-
+    #method to add another proposed_stiffener
     def add(self, proposed_stiffener):
         self.stiffeners.append(proposed_stiffener)
         return
-
+    #method to get a proposed_stiffener using position and number
     def get_proposed_stiffener(self, pl_position, st_number):
         for prop_stiffener in self.stiffeners:
             if prop_stiffener.pl_position == pl_position and prop_stiffener.st_number == st_number:
                 return prop_stiffener
         return None
-
-    def print_stiffeners_proposition(self):
-        if defaults.do_print_to_txt == True:
-            file = open("output\cs_analysis.txt", "a+")
-            file.write("\nSTIFFENERS PROPOSITION")
-            for stiffener in self.stiffeners:
-                stiffener.print_stiffener()
-            file.close()
-        if defaults.do_print_to_terminal == True:
-            pass
