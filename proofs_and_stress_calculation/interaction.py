205371aba42b84116566526495f09eabdf00ceeb
nhasler789 <77466310+nhasler789@users.noreply.github.com>
2021-05-25 09:52:24 +0200

    absolute values for eta

--- proofs_and_stress_calculation/interaction.py
+++ proofs_and_stress_calculation/interaction.py
@@ -35,9 +35,10 @@ def interaction_web(total_cs, web_plate, eta_3):
         #interaction required
         plastic_cs = copy.deepcopy(total_cs)
         m_pl_rd = get_m_rd_pl_eff(plastic_cs)
-        eta_1 = m_ed / m_pl_rd
+        eta_1 = abs(m_ed) / m_pl_rd
         utilisation = eta_1 + (1-m_f_rd/m_pl_rd)*(2*eta_3-1)**2
         line2 = "\n      utilisation: "+str(utilisation)
+        string = line1 + line2
         printing.printing(string, terminal = True)
         return utilisation
 
