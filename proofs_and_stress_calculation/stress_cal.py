7624278e05f1fbbb6f5caec07d8f33cfc41ce710
muellvin <78660277+muellvin@users.noreply.github.com>
2021-05-26 16:40:48 +0200

    a

--- proofs_and_stress_calculation/stress_cal.py
+++ proofs_and_stress_calculation/stress_cal.py
@@ -134,14 +134,14 @@ def get_tau_int_t(cs, side, t_ed):
     tau_int = tau * length
     return tau_int
 
-#for S_y and I_y stress = True, thus area also needs to be with t_stress
-#else total force way too high
+#for S_y and I_y stress = False, come from moment equilibrium
 def get_tau_int_flange(cs, side, v_ed, t_ed):
+    stress = False
     area = 0
     t = cs.get_line(pl_position = side, pl_type = 0).t
     for plate in cs.lines:
         if plate.code.pl_type == 0 and plate.code.pl_position == side:
-            area += plate.get_length_tot()*plate.t_stress
+            area += plate.get_length_tot()*plate.t
     tau_int_t_flange = get_tau_int_t(cs, side, t_ed)
     #calculate exact shear stresses due to shear force
     if side == 1:
@@ -150,8 +150,8 @@ def get_tau_int_flange(cs, side, v_ed, t_ed):
         b = cs.b_inf
     else:
         assert True, "This should never happen!"
-    S_y_corner = b*0.5*t*abs(cs.get_center_z_tot(stress = True)-cs.get_line(pl_position = side, pl_type = 0).a.z)
-    tau_q_max_abs = abs(v_ed * S_y_corner /(t * cs.get_i_y_tot(stress = True)))
+    S_y_corner = 0.5*b*t*abs(cs.get_center_z_tot(stress)-cs.get_line(pl_position = side, pl_type = 0).a.z)
+    tau_q_max_abs = abs(0.5*v_ed * S_y_corner /(t * cs.get_i_y_tot(stress)))
     tau_int_qy_flange = tau_q_max_abs * area * 0.5
     tau_int_flange = max(abs(tau_int_t_flange+tau_int_qy_flange), abs(tau_int_t_flange-tau_int_qy_flange))
 
@@ -162,26 +162,27 @@ def get_tau_int_flange(cs, side, v_ed, t_ed):
     return tau_int_flange
 
 def get_tau_int_subpanel(cs, panel, v_ed, t_ed):
+    stress = False
     #check if plate is flange plate
     assert panel.code.pl_position == 1 or panel.code.pl_position == 3, "ERROR!!"
     #calculate tau_mean from q
-    center_of_panel = panel.get_center_y_tot(stress = True)
+    center_of_panel = panel.get_center_y_tot(stress)
     if center_of_panel == 0:
         #plate where tau changes sign
-        x = panel.get_length_tot(stress = True)/4
+        x = panel.get_length_tot(stress)/4
     else:
         x = abs(center_of_panel)
-    S_y_panel = x*panel.t_stress*abs(cs.get_center_z_tot(stress = True)-panel.a.z)
-    tau_q_panel_abs = abs(v_ed * S_y_panel /(panel.t_stress * cs.get_i_y_tot(stress = True)))
-    tau_int_qy_panel = panel.get_area_tot(stress = True)*tau_q_panel_abs
+    S_y_panel = x*panel.t*abs(cs.get_center_z_tot(stress)-panel.a.z)
+    tau_q_panel_abs = abs(v_ed * S_y_panel /(panel.t * cs.get_i_y_tot(stress)))
+    tau_int_qy_panel = panel.get_area_tot(stress)*tau_q_panel_abs
 
     #calculate tau_mean from t
     side = panel.code.pl_position
     tau_int_t_flange = get_tau_int_t(cs, side, t_ed)
     tau_int_t_panel = 0
     if panel.code.pl_position == 1:
-        tau_int_t_panel = panel.get_length_tot(stress = False)/cs.b_sup*tau_int_t_flange
+        tau_int_t_panel = panel.get_length_tot(stress)/cs.b_sup*tau_int_t_flange
     else:
-        tau_int_t_panel = panel.get_length_tot(stress = False)/cs.b_inf*tau_int_t_flange
+        tau_int_t_panel = panel.get_length_tot(stress)/cs.b_inf*tau_int_t_flange
     tau_int_panel = max(abs(tau_int_t_panel+tau_int_qy_panel), abs(tau_int_t_panel-tau_int_qy_panel))
     return tau_int_panel
